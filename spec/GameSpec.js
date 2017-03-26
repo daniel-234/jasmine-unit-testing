b700ad28a623818b60c68f0a86426b69f2394b07
Daniele <daniele.rb6@gmail.com>
2017-03-26 16:38:42 +0200

    feat: Write the tests
    Wrote tests that equated the descriptions given in Planning.txt for the functions needed to move the player in the game.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,9 +1,42 @@
 // Here is where your tests will go.
+describe('The game', function() {
+	// write a test describing the function called moveRight
+	describe('The function called moveRight', function() {
+		it('should add 1 to player.x', function() {
+			// Store the player x coordinate before moving it to the right
+			var xCoordinate = player.x;
+			// Move player to the right
+			moveRight();
+			// Expect its new x coordinate to be equal to its former
+			// x coordinate value before calling the moveRight function
+			expect(player.x).toEqual(xCoordinate + 1);
+		});
+	});
 
-// write a test describing the function called moveRight
+	// write a test describing the function called moveLeft
+	describe('The function called moveLeft', function() {
+		it('should subtract 1 from player.x', function() {
+			var xCoordinate = player.x;
+			moveLeft();
+			expect(player.x).toEqual(xCoordinate - 1);
+		});
+	});
 
-// write a test describing the function called moveLeft
+	// write a test describing the function called moveDown
+	describe('The function called moveDown', function() {
+		it('should add 1 to player.y', function() {
+			var yCoordinate = player.y;
+			moveDown();
+			expect(player.y).toEqual(yCoordinate - 1);
+		});
+	});
 
-// write a test describing the function called moveDown
-
-// write a test describing the function called moveUp
+	// write a test describing the function called moveUp
+	describe('The function called moveUp', function() {
+		it('should subtract 1 from player.y', function() {
+			var yCoordinate = player.y;
+			moveUp();
+			expect(player.y).toEqual(yCoordinate + 1);
+		});
+	});
+});
